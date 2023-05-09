98a43eca972db02769f8ac2e187028c523f4f926
elichartnett <elichartnett@icloud.com>
2023-05-09 07:46:12 -0400

    Initial Commit

---
+++ ParticleEmitter/ParticleEmitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleEmitterApp.swift
+//  ParticleEmitter
+//
+//  Created by Eli Hartnett on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ParticleEmitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
